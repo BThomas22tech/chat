b0b7643f49f6615accefc9658ca7c16149da1c61
BThomas <bran_thom@yahoo.com>
2023-06-01 11:34:55 -0500

    django chat with ignore

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+from . import keys
 
+openai.api_key = keys.openai_api_key
 # Create your views here.
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
 def chatbot(request):
     if request.method =='POST':
-        
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
+
     return render(request, 'chatbot.html')
